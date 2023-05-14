5acd1de61300e01d1f8ba13a28139766b9d37a1d
79998 <lekha-fadeev@mail.ru>
2023-05-14 22:39:31 +0300

    коммит

---
+++ src/Author.java
@@ -0,0 +1,19 @@
+public class Author {
+
+    private String name;
+
+    private String secondName;
+
+    public Author(String name, String secondName) {
+        this.name = name;
+        this.secondName = secondName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+}
